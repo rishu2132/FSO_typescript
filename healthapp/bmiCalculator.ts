bcd6456efae4006fdceee6194f52d5aeaea80795
Utkarsh <utk1298@gmail.com>
2026-08-25 20:42:31 +0530

    Exercise 4: Express

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -25,9 +25,9 @@ const calculateBmi = (height: number, weight: number) => {
 
     const bmi = weight / ((height/100)**2);
     if (bmi < 18.5) return 'Underweight';
-    if (bmi >= 18.5 && bmi <=25) return 'Normal range';
-    if (bmi > 25 && bmi <= 30) return 'Overweight';
-    if (bmi > 30) return 'Obese';
+    else if (bmi >= 18.5 && bmi <=25) return 'Normal range';
+    else if (bmi > 25 && bmi <= 30) return 'Overweight';
+    else return "Obese";
 }
 
 try {
